d9462b4746d2f7ff330a3f68969074a433c73348
Oscar Riascos <osferibur@hotmail.com>
2022-01-15 14:00:13 -0500

    Add a next.config example file

---
+++ example.next.config.js
@@ -0,0 +1,16 @@
+/**
+ * Remove "example." word from this file name
+*/
+
+module.exports = {
+  reactStrictMode: true,
+  env: {      
+    APP_NAME: 'Your app name :: ',
+    RECIPIENT_NAME: 'Sendingblue recipient name',
+    RECIPIENT_EMAIL: 'Sendingblue recipient mail',
+    DEFAULT_SUBJECT: 'Default Subject',//Optional
+    SENDIN_BLUE_API_KEY: 'YOUR_SENDINBLUE_API_KEY',
+    SENDIN_BLUE_API: 'SENDINBLUE_API_ENDPOINT',
+    GCAPTCHA_KEY: 'GOOGLE_RECAPTCHA_API_KEY'
+  }
+}
