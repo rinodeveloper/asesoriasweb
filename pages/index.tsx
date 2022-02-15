9db7fe638dc2b9bf9795aaddbb9021ba7d2dafdc
Oscar <osferibur@hotmail.com>
2022-02-15 11:20:35 -0500

    Encoding minor error fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           className={'container d-flex flex-wrap flex-row'}
         >
         <section className={'col-12 col-md-7 col-sm-12 d-flex flex-column align-self-center'}>
-          <h1 className={'accent'}>I'm Oscar Riascos</h1>
+          <h1 className={'accent'}>I&apos;m Oscar Riascos</h1>
           <h3>
             Mi objetivo es ayudarte a aprovechar la tecnología.
           </h3>
@@ -28,7 +28,7 @@ export default function Home() {
                 >
                 Servicios 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ffffff" className="bi bi-chevron-double-down" viewBox="0 0 16 16">
-                  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+                  <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
                 </svg>
                 </a>
             </div>
